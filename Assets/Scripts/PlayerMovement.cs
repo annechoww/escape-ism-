ecdfd15aff2f29faa24fd99fbdac98cd2f3bdb91
Anne Chow <anne.chow123@hotmail.com>
2025-01-13 11:46:24 -0500

    Initial commit for Unity project

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerMovement2D : MonoBehaviour
+{
+    // Globals
+    private Rigidbody2D playerRb;
+    public float playerSpeed = 5f;
+    private float moveX, moveY;
+    private Animator animator;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveX = Input.GetAxis("Horizontal");
+        moveY = Input.GetAxis("Vertical");
+
+        // Update Animator parameters
+        animator.SetFloat("Horizontal", moveX);
+        animator.SetFloat("Vertical", moveY);
+        animator.SetBool("IsMoving", Mathf.Abs(moveX) > 0 || Mathf.Abs(moveY) > 0);
+    }
+
+    void FixedUpdate()
+    {
+        playerRb.linearVelocity = new Vector2(moveX, moveY) * playerSpeed;
+    }
+}
