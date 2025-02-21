6e5a679a3514cb86d41dee532fa7d2d3185a5713
Anne Chow <anne.chow123@hotmail.com>
2025-02-20 22:55:12 -0500

    Added more coins to scene. Added coin audio

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -2,12 +2,14 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
+    public AudioSource audioSource;
     
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             other.GetComponent<Player>().UpdateCoinCount();
+            audioSource.Play();
             Destroy(gameObject);
         }
     }
